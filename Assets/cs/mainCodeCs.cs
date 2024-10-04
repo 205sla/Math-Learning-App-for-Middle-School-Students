da1bd39137271980afa8f62ddde1310e72881aee
YOUNGHO LEE <61982040+205sla@users.noreply.github.com>
2024-10-04 20:07:38 +0900

    폰트 추가 엑셀 불러오기 개발 완료

--- Assets/cs/mainCodeCs.cs
+++ Assets/cs/mainCodeCs.cs
@@ -1,57 +1,90 @@
-using System; 
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class mainCodeCs : MonoBehaviour
 {
-    [Serializable]
-    public class Lotto
+    public class BasicItem
     {
-        public int index;
-        public string step;
-        public string problem;
-        public string correctAnswer;
-        public string wrongAnswer1;
-        public string wrongAnswer2;
-        public string wrongAnswer3;
-        public string wrongAnswer4;
-        public string wrongAnswer5;
-        public string wrongAnswer6;
-
-        public string img;
-        public int[] aaa;
-        public int[] bbb;
-        public int[] ccc;
+        public int index; //번호
+        public int semester; //학기
+        public string type; //단원
+        public string question; //문제
+        public string img; //이미지
+    }
 
 
-        public void printNumbers()
+    [Serializable]
+    public class OXQuiz : BasicItem
+    {
+        public string correct_answer; //답 OX
+        public object[] LoadProblem()
         {
+            return new object[] { index, semester, type, question, img, correct_answer };
+        }
 
-            Debug.Log("bonus : " + problem);
+    }
+    [Serializable]
+    public class short_answer : BasicItem
+    {
+        public string correct_answer; //답
+        public object[] LoadProblem()
+        {
+            return new object[] { index, semester, type, question, img, correct_answer };
+        }
+    }
+    [Serializable]
+    public class select_one : BasicItem
+    {
+        public string correct_answer; //답
+        public string wrong_answer1; //오답1
+        public string wrong_answer2; //오답2
+        public string wrong_answer3; //오답3
+        public string wrong_answer4; //오답4 
+
+        public object[] LoadProblem()
+        {
+            return new object[] { index, semester, type, question, img, correct_answer, wrong_answer1, wrong_answer2, wrong_answer3, wrong_answer4 };
         }
     }
 
-    public class LottoNumbers
+
+
+    [Serializable]
+    public class select_multiple : BasicItem
     {
-        public Lotto[] winning;
+        public string number_of_correct_answers; //답
+        public string answer1;
+        public string answer2;
+        public string answer3;
+        public string answer4;
+        public string answer5;
+
+        public object[] LoadProblem()
+        {
+            return new object[] { index, semester, type, question, img, number_of_correct_answers, answer1, answer2, answer3, answer4, answer5 };
+        }
+
+
+    }
+
+    public class FullQuestion
+    {
+        public OXQuiz[] OXQuiz;
+        public short_answer[] short_answer;
+        public select_one[] select_one;
+        public select_multiple[] select_multiple;
     }
 
     void Start()
     {
         TextAsset textAsset = Resources.Load<TextAsset>("Json/mathProblem");
 
-        LottoNumbers lottoList = JsonUtility.FromJson<LottoNumbers>(textAsset.text);
-        Debug.Log(lottoList);
-        foreach (Lotto lt in lottoList.winning)
+        FullQuestion QuestionList = JsonUtility.FromJson<FullQuestion>(textAsset.text);
+        foreach (OXQuiz lt in QuestionList.OXQuiz)
         {
-            lt.printNumbers();
-            Debug.Log("=============");
+            Debug.Log("번호: "+ lt.LoadProblem()[0] +"학기: "+ lt.LoadProblem()[1]+"단원: "+ lt.LoadProblem()[2]+"문제"+ lt.LoadProblem()[3]);
+
         }
-        /*
-        string classToJson = JsonUtility.ToJson(lottoList);
-        Debug.Log(classToJson);
-        */
     }
 
 }
